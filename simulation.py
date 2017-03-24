95d99e639e23012034376f10632c2431c006b1fe
v.adhithyan <aavispeaks@gmail.com>
2017-03-24 22:17:01 +0530

    all code blocks moved to appropriate functions

--- simulation.py
+++ simulation.py
@@ -7,41 +7,44 @@ variables = ["age", "weight", "sbp", "glucose", "cardio_events", "nephro_level",
 variables_init = ["0", "0", "0", "0", "0", "low", "null"]
 simulated_values = []
 simulation_length = 975
-
-for i in range(simulation_length):
-    dummy = {}
-    for j in range(len(variables)):
-        dummy[variables[j]] = variables_init[j]
-    simulated_values.append(dummy)
-#print simulated_values
-
 real_values = []
-with open("input.csv", "rb") as input_file:
-    reader = csv.DictReader(input_file)
-    for row in reader:
-        real_values.append(row)
-
 acceptance_rate = 0
 error = 0.08
 
-t1 = time.clock()
-
-for beta in range(25):
+def init():
     for i in range(simulation_length):
-        for alpha in range(7):
-            aleatory_number = random.random()
-            gaussian_profile = numpy.random.normal()
-            if gaussian_profile > aleatory_number:
-                acceptance_rate = acceptance_rate + 1
-                acceptance = gaussian_profile + error
-                simulated_values[i][variables[alpha]] = real_values[beta][variables[alpha]]
+        dummy = {}
+        for j in range(len(variables)):
+            dummy[variables[j]] = variables_init[j]
+            simulated_values.append(dummy)
 
-t2 = time.clock()
+def read_input():
+    with open("input.csv", "rb") as input_file:
+        reader = csv.DictReader(input_file)
+        for row in reader:
+            real_values.append(row)
 
-with open("output.csv", "wb") as f:
-    writer = csv.DictWriter(f, fieldnames = variables)
-    writer.writeheader()
-    writer.writerows(simulated_values)
+def save_simulated_values():
+    with open("output.csv", "wb") as f:
+        writer = csv.DictWriter(f, fieldnames = variables)
+        writer.writeheader()
+        writer.writerows(simulated_values)
 
-print "The total time is {} seconds".format(t2-t1)
-print "The acceptance rate is {}".format(acceptance_rate)
+def monte_carlo_simulation():
+    for beta in range(25):
+        for i in range(simulation_length):
+            for alpha in range(7):
+                aleatory_number = random.random()
+                gaussian_profile = numpy.random.normal()
+                if gaussian_profile > aleatory_number:
+                    #acceptance_rate = acceptance_rate + 1
+                    #acceptance = gaussian_profile + error
+                    simulated_values[i][variables[alpha]] = real_values[beta][variables[alpha]]
+
+def main():
+    init()
+    read_input()
+    monte_carlo_simulation()
+    save_simulated_values()
+
+main()
