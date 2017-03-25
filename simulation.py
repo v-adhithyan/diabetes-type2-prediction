8ad6a20e2cc2cad3e6e406d2084ab65c37a5f8a5
v.adhithyan <aavispeaks@gmail.com>
2017-03-25 22:45:45 +0530

    one prediction method added

--- simulation.py
+++ simulation.py
@@ -1,4 +1,6 @@
+import argparse
 import csv
+import matplotlib.pyplot as plt
 import numpy
 import random
 import time
@@ -7,44 +9,167 @@ variables = ["age", "weight", "sbp", "glucose", "cardio_events", "nephro_level",
 variables_init = ["0", "0", "0", "0", "0", "low", "null"]
 simulated_values = []
 simulation_length = 975
+
+for i in range(simulation_length):
+    dummy = {}
+    for j in range(len(variables)):
+        dummy[variables[j]] = variables_init[j]
+    simulated_values.append(dummy)
+#print simulated_values
+
 real_values = []
+with open("input.csv", "rb") as input_file:
+    reader = csv.DictReader(input_file)
+    for row in reader:
+        real_values.append(row)
+
 acceptance_rate = 0
 error = 0.08
 
-def init():
-    for i in range(simulation_length):
-        dummy = {}
-        for j in range(len(variables)):
-            dummy[variables[j]] = variables_init[j]
-            simulated_values.append(dummy)
+def show_histogram(input):
+    x = []
+    y = []
+    for k, v in input.items():
+        x.append(k)
+    x.sort()
 
-def read_input():
-    with open("input.csv", "rb") as input_file:
-        reader = csv.DictReader(input_file)
-        for row in reader:
-            real_values.append(row)
+    for val in x:
+        y.append(input[val])
 
-def save_simulated_values():
-    with open("output.csv", "wb") as f:
-        writer = csv.DictWriter(f, fieldnames = variables)
-        writer.writeheader()
-        writer.writerows(simulated_values)
+    hist, bins = numpy.histogram(y, bins = x)
+    width = numpy.diff(bins)
+    center = (bins[:-1] + bins[1:]) / 2
 
-def monte_carlo_simulation():
+    #fig, ax = plt.subplots(figsize=(8,3))
+    #x.bar(center, hist, align='center', width=width)
+    #x.set_xticks(bins)
+    #fig.savefig("/tmp/out.png")
+    #plt.hist(y, bins = [40] + x)
+
+    plt.show()
+
+def is_depression(val):
+    return val > 0 and val <= 0.33
+
+def is_necrosis(val):
+    return val > 0.33 and val <= 0.66
+
+def is_nephropathy(val):
+    return val > 0.66 and val <= 1.00
+
+def d_z(val):
+    x11 = 10.0 ** 2
+    x12 = 10.0 ** 2
+    x13 = 70.0
+    x14 = 2.0
+    dis = (val - x13) / x14
+    dis = dis ** 2
+
+    return x11 / (x12 + dis)
+
+def w_z(val):
+    x21 = 100.0
+    x22 = 55.0
+    x23 = 40.0
+    dis = ((val - x22) / x23) ** 2
+    dis = math.exp(-dis)
+
+    return x21 * dis
+
+def g_z(val):
+    x31 = 150.0
+    x32 = 0.3
+    x33 = 440.0
+    dis = random.random() * (val - 30) / 0.05
+
+    return (x31 + (dis * x32)) / x33
+
+def c_z(val):
+    x51 = 2.1
+    x52 = 0.5
+    x53 = 0.00066
+    x54 = 3.0
+    z = x52 + (x53 * ((val - 30) / 0.05))
+    c = 1 + ((2 * random.random()) ** x54)
+
+    return (z * c) / x51
+
+def necrosis_wrto_glucose_and_depression():
+    count = 0
+    #print "The length of sim values is {}".format(len(simulated_values))
+    age_count = {}
+    age_count[50] = 0
+    age_count[60] = 0
+    age_count[70] = 0
+    age_count[80] = 0
+
+    for m in range(simulation_length):
+        nj = random.randint(0, simulation_length - 1)
+        #print nj
+        patient = simulated_values[nj]
+        age = int(patient["age"])
+        #weight = int(patient["weight"])
+        glucose = int(patient["glucose"])
+
+        if age >= 30 and age <=80:
+            zk = age
+            if d_z(age) > 0.5:
+                if g_z(glucose) > 0.5:
+                    if c_z(age) > 0.66:
+                        #print age
+                        if age >= 50 and age < 60:
+                            print "50"
+                            age_count[50] = age_count[50] + 1
+                        if age >= 60 and age < 70:
+                            print "60"
+                            age_count[60] = age_count[60] + 1
+                        if age >= 70 and age < 80:
+                            print "70"
+                            age_count[70] = age_count[70] + 1
+                        if age >= 80:
+                            print "80"
+                            age_count[80] = age_count[80] + 1
+
+    print age_count
+    show_histogram(age_count)
+    #print "The number of critic patients is {}".format(count)
+
+def identify_critical_patients():
+    necrosis_wrto_glucose_and_depression()
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--simulate", help = "Run a new monte carlo simulation and use new values for prediction", action = "store_true")
+args = parser.parse_args()
+
+#t1 = time.clock()
+
+if args.simulate:
+    print "New simulation ..."
     for beta in range(25):
         for i in range(simulation_length):
             for alpha in range(7):
                 aleatory_number = random.random()
                 gaussian_profile = numpy.random.normal()
                 if gaussian_profile > aleatory_number:
-                    #acceptance_rate = acceptance_rate + 1
-                    #acceptance = gaussian_profile + error
+                    acceptance_rate = acceptance_rate + 1
+                    acceptance = gaussian_profile + error
                     simulated_values[i][variables[alpha]] = real_values[beta][variables[alpha]]
 
-def main():
-    init()
-    read_input()
-    monte_carlo_simulation()
-    save_simulated_values()
+#t2 = time.clock()
 
-main()
+    with open("output.csv", "wb") as f:
+        writer = csv.DictWriter(f, fieldnames = variables)
+        writer.writeheader()
+        writer.writerows(simulated_values)
+
+    identify_critical_patients()
+
+else:
+    print "Read simulated values ...."
+    with open("output.csv", "r") as f:
+        reader = csv.DictReader(f)
+
+        for row in reader:
+            print row
